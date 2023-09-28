f8a3d6401d43c64f2443fdb983126fe02db05746
Bruno Santos <bp.santosesantos@gmail.com>
2023-09-28 16:54:14 -0300

    feat: App refactoring to use Navigation API

--- app/src/main/java/com/example/electrocarapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/electrocarapp/activity/MainActivity.kt
@@ -5,59 +5,38 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.electrocarapp.R
 import com.example.electrocarapp.adapter.TabAdapter
+import com.example.electrocarapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.tabs.TabLayout
 import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tlFragments: TabLayout
-    private lateinit var vpFragments: ViewPager2
-    private lateinit var fabCalculate: FloatingActionButton
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        // Inits
-        inits()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bnvNavigation, navController)
+
         setupListeners()
     }
 
-    private fun inits() {
-        this.tlFragments = findViewById(R.id.tl_fragments)
-
-        this.vpFragments = findViewById(R.id.vp_fragments)
-        this.vpFragments.adapter = TabAdapter(this)
-
-        this.fabCalculate = findViewById(R.id.fab_calculate)
-    }
-
     private fun setupListeners() {
-        this.tlFragments.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                tab?.let {
-                    vpFragments.currentItem = it.position
-                }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-        })
-
-        this.vpFragments.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                tlFragments.getTabAt(position)?.select()
-            }
-        })
-
-        this.fabCalculate.setOnClickListener {
+        binding.fabCalculate.setOnClickListener {
             goToCalculateAutonomy()
         }
     }
